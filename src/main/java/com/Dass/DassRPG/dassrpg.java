f25f05afe722f0441a8258e8e1509b53ea97e9fa
LoadArt <loadart.developer@gmail.com>
2022-04-08 13:14:24 +0300

    Initial commit

---
+++ src/main/java/com/Dass/DassRPG/dassrpg.java
@@ -0,0 +1,35 @@
+package com.Dass.DassRPG;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(dassrpg.MOD_ID)
+public class dassrpg
+{
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final String MOD_ID = "dassrpg";
+
+    public dassrpg()
+    {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        // some preinit code
+
+    }
+
+}
